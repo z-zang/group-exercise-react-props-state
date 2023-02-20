11741bdf3fda907e0e87fba2d1e1622acd416e8d
EllieJudge <47675388+EllieJudge@users.noreply.github.com>
2023-02-20 17:52:47 +0000

    Update chuck_card.tsx

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,16 +1,22 @@
 // 💡 You can import assets like images directly and use them in your components:
 import ChuckImage from '../assets/chuck_norris.jpg';
 
-const ChuckCard = () =>
-	// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
-	// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
-	<>
+interface ChuckCardProps {
 
-	{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
-		<h2>{ }</h2>
+}
 
-		<img src={'Chuck image here!'} alt='A handsome man' />
-	</>
+const ChuckCard: React.FC<ChuckCardProps> = ({}) => {
 
+	return (
+		// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
+		// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
+		<>
+			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
+			<h2>{ }</h2>
+
+			<img src={'Chuck image here!'} alt='A handsome man' />
+		</>
+	)
+}
 
 export default ChuckCard;
